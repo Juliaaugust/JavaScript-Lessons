df3ef1f6d6e6028511ebe7d3316d24efb4b80431
Julia <juliaaugust96@gmail.com>
2018-08-09 21:35:54 +0300

    Consider space

--- Practic_1_1/script.js
+++ Practic_1_1/script.js
@@ -2,9 +2,16 @@ var userStr = prompt('Enter the string');
 var arr = userStr.split('');
 console.log(arr);
 for(var i=0; i<arr.length; i++){
-	if(i%2==0){
-		document.write('<div class="even" align="center">' + arr[i]+'</div><br/>');
-	} else {
-		document.write('<div class="uneven" align="center">' + arr[i]+'</div><br/>');
+
+	switch(arr[i]) {
+		case ' ': document.write('<div class="space"> </div> ');
+		break;
+
+		default:
+		if(i%2==0){
+			document.write(' <div class="even" align="center">' + arr[i]+'</div> ');
+		} else {
+			document.write(' <div class="uneven" align="center">' + arr[i]+'</div> ');
+		}
 	}
 }
